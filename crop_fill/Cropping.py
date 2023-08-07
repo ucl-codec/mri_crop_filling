bc5089c360f67ca9e0dafe8c6cbee45f717c59a0
GonCasLeal <99740687+GonCasLeal@users.noreply.github.com>
2023-08-07 12:39:47 +0100

    Add files via upload

---
+++ crop_fill/Cropping.py
@@ -0,0 +1,197 @@
+import nibabel as nb
+import numpy as np
+from skimage.exposure import match_histograms
+
+
+def get_sub_list(file):
+    with open(file) as f:
+        sub_list = f.readlines()
+    f.close()
+    return sub_list
+
+
+def get_nii_info(img_path):
+    img = nb.load(img_path)
+    affine = img.affine
+    img_fdata = img.get_fdata()
+    header = img.header
+    return affine, img_fdata, header
+
+
+def save_img(affine, img_fdata, header, path):
+    cropped_img = nb.Nifti1Image(img_fdata, affine, header)
+    nb.save(cropped_img, path)
+
+
+def get_first_and_last_zero(array_sample):
+    for i in range(len(array_sample)):
+        if array_sample[i - 1] != 0:
+            first_non_zero = i
+            break
+
+    i = len(array_sample) - 1
+    while i >= 0:
+        if array_sample[i] != 0:
+            last_non_zero = i
+            break
+        i -= 1
+    return first_non_zero, last_non_zero
+
+
+def crop_img(img_path, img_cropped_path):
+    affine, img_fdata, header = get_nii_info(img_path)
+    middle_point = np.floor(np.shape(img_fdata)[2] / 2).astype(np.int)
+
+    _, _, z_dims = header.get_zooms()
+    img_center_slices = np.floor(120 / z_dims).astype(np.int)
+    img_nozero_slices = np.floor(img_center_slices / 2).astype(np.int)
+
+    min = middle_point - img_nozero_slices
+    max = middle_point + img_nozero_slices
+    img_cropped = img_fdata[:, :, :]
+    img_cropped[:, :, :min] = 0
+    img_cropped[:, :, max:] = 0
+    save_img(affine, img_cropped, header, img_cropped_path)
+
+
+def collage_img(img_cropped_path, img_add_path, img_frankestein_path):
+    affine, img_cropped_fdata, header = get_nii_info(img_cropped_path)
+    _, img_add_fdata, _ = get_nii_info(img_add_path)
+
+    # Rescale Intensity
+    for j in np.arange(np.shape(img_cropped_fdata)[1]):
+        img_add_fdata[:, j, :] = match_histograms(img_add_fdata[:, j, :], img_cropped_fdata[:, j, :])
+    img_frankestein = img_cropped_fdata
+    first_non_zero, last_non_zero = get_first_and_last_zero(img_frankestein[128, 128, :])
+    img_frankestein[:, :, :first_non_zero - 1] = img_add_fdata[:, :, :first_non_zero - 1]
+    img_frankestein[:, :, last_non_zero + 1:] = img_add_fdata[:, :, last_non_zero + 1:]
+    img_frankestein_path = img_frankestein_path
+    save_img(affine, img_frankestein, header, img_frankestein_path)
+
+
+def zero_pad(img_pad_path, img_reference, img_zeropadded_path):
+    affine_pad, img_pad_fdata, header_pad = get_nii_info(img_pad_path)
+    _, img_ref_fdata, header_ref = get_nii_info(img_reference)
+
+    img_ref_shape = np.shape(img_ref_fdata)
+    img_ref_dims = header_ref.get_zooms()
+    z_ref_fov = img_ref_shape[0] * img_ref_dims[0]
+    print(z_ref_fov, img_ref_dims, img_ref_shape)
+
+    img_pad_shape = np.shape(img_pad_fdata)
+    img_pad_dims = header_pad.get_zooms()
+    z_pad_fov = img_pad_shape[0] * img_pad_dims[0]
+    print(z_pad_fov, img_pad_shape, img_pad_dims)
+
+    diff_in_mm = z_ref_fov - z_pad_fov
+    zeros_add = np.round(diff_in_mm / img_pad_dims[2])
+    zeros_add = np.floor(zeros_add / 2.) * 2
+    padded_shape = np.array([img_pad_shape[0] + zeros_add, img_pad_shape[1], img_pad_shape[2]]).astype(np.int)
+    img_padded_fdata = np.zeros(padded_shape)
+    min = np.int(zeros_add / 2)
+    max = np.int(padded_shape[0] - min)
+    img_padded_fdata[min:max, :, :] = img_pad_fdata
+
+    print(affine_pad)
+    affine_pad[:, 3] = [affine_pad[0, 3] - zeros_add / 2, affine_pad[1, 3] - 1, affine_pad[2, 3] + 2, affine_pad[3, 3]]
+    print(affine_pad)
+    img_padded = nb.Nifti1Image(img_padded_fdata, affine_pad)
+    nb.save(img_padded, img_zeropadded_path)
+
+
+def trim(img_cropped_path, img_ds_path, trim_image_path):
+    img_cropped_affine, img_cropped_fdata, img_cropped_header = get_nii_info(img_cropped_path)
+    img_ds_affine, img_ds_fdata, img_ds_header = get_nii_info(img_ds_path)
+
+    img_cropped_shape = np.shape(img_cropped_fdata)
+    img_ds_shape = np.shape(img_ds_fdata)
+
+    abc_cropped = np.around(img_cropped_affine[:3, 3], 2)
+    abc_ds = np.around(img_ds_affine[:3, 3], 2)  # Translation vector of the image, for the case of the [0, 0, 0]
+    # voxels it tells how far it is from the center of the image in spatial coordinates (0, 0, 0)
+
+    img_dims = img_ds_header.get_zooms()  # Dimension of the voxels in each direction,
+    # it assumes that both  images have the same dimensions
+
+    # For all the axis we assume that if the dimensions of both matrices are not the exact same,
+    # and the [0, 0, 0] voxels of both images are at a similar distance from the spatial center, then the error in
+    # matrix shape is due to extra voxels in the far ends of the matrices.
+
+    diff_abc = abs(abc_cropped) - abs(abc_ds)
+    new_shape = img_ds_shape
+    print(new_shape, img_cropped_shape)
+    num_slices_trimx, num_slices_addx, num_slices_trimmax_x, num_slices_add2x, new_shape = get_trims(img_ds_shape[0],
+                                                                                                     img_cropped_shape[
+                                                                                                         0],
+                                                                                                     diff_abc[0],
+                                                                                                     img_dims[0],
+                                                                                                     new_shape,
+                                                                                                     0)
+
+    num_slices_trimy, num_slices_addy, num_slices_trimmax_y, num_slices_add2y, new_shape = get_trims(img_ds_shape[1],
+                                                                                                     img_cropped_shape[
+                                                                                                         1],
+                                                                                                     diff_abc[1],
+                                                                                                     img_dims[1],
+                                                                                                     new_shape,
+                                                                                                     1)
+
+    num_slices_trimz, num_slices_addz, num_slices_trimmax_z, num_slices_add2z, new_shape = get_trims(img_ds_shape[2],
+                                                                                                     img_cropped_shape[
+                                                                                                         2],
+                                                                                                     diff_abc[2],
+                                                                                                     img_dims[2],
+                                                                                                     new_shape,
+                                                                                                     2)
+    print(new_shape)
+    new_image = np.zeros(new_shape)
+    new_image[num_slices_addx:new_shape[0] - num_slices_add2x, num_slices_addy:new_shape[1] - num_slices_add2y,
+              num_slices_addz:new_shape[2] - num_slices_add2z] = img_ds_fdata[
+                                                               num_slices_trimx:img_ds_shape[0] - num_slices_trimmax_x,
+                                                               num_slices_trimy:img_ds_shape[1] - num_slices_trimmax_y,
+                                                               num_slices_trimz:img_ds_shape[2] - num_slices_trimmax_z]
+
+    save_img(img_cropped_affine, new_image, img_cropped_header, trim_image_path)
+
+
+def get_trims(img_ds_shape, img_cropped_shape, diff_abc, img_dims, new_shape, ind):
+    num_slices_trim = 0
+    num_slices_add = 0
+    num_slices_trimmax = 0
+    num_slices_add2 = 0
+    new_image = np.zeros(new_shape)
+    if img_ds_shape != img_cropped_shape:
+        if abs(diff_abc) > img_dims / 2 and diff_abc < 0:
+            num_slices_add = np.round(abs(diff_abc / img_dims)).astype(int)
+            print("Slice to add at the beginning: ", num_slices_add)
+            add_ar = np.array([0, 0, 0])
+            add_ar[ind] = num_slices_add
+            new_image = np.zeros(new_shape + add_ar)
+        elif abs(diff_abc) > img_dims / 2 and diff_abc > 0:
+            num_slices_trim = np.round(abs(diff_abc / img_dims)).astype(int)
+            add_ar = np.array([0, 0, 0])
+            add_ar[ind] = num_slices_trim
+            new_image = np.zeros(new_shape - add_ar)
+            print("Slices to delete from the beginning: ", num_slices_trim)
+        else:
+            new_image = np.zeros(new_shape)
+
+        new_shape = np.shape(new_image)
+        print(new_shape)
+        if new_shape[ind] != img_cropped_shape:
+            if new_shape[ind] > img_cropped_shape:
+                num_slices_trimmax = new_shape[ind] - img_cropped_shape
+                add_ar = np.array([0, 0, 0])
+                add_ar[ind] = num_slices_trimmax
+                new_image = np.zeros(new_shape - add_ar)
+                print("Slices to delete from the end: ", num_slices_trimmax)
+            else:
+                num_slices_add2 = img_cropped_shape - new_shape[ind]
+                add_ar = np.array([0, 0, 0])
+                add_ar[ind] = num_slices_add2
+                new_image = np.zeros(new_shape + add_ar)
+                print("Slices to add at the end: ", num_slices_add2)
+
+    new_shape = np.shape(new_image)
+
+    return num_slices_trim, num_slices_add, num_slices_trimmax, num_slices_add2, new_shape
---
+++ crop_fill/run.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+import argparse
+import os
+from glob import glob
+from Cropping import crop_img, collage_img, zero_pad, trim
+
+parser = argparse.ArgumentParser(description='FreeSurfer recon-all + custom template generation.')
+
+parser.add_argument('bids_dir', help='The directory with the input dataset '
+                                     'formatted according to the BIDS standard.')
+parser.add_argument('--participant_label', help='The label of the participant that should be analyzed. The label '
+                                                'corresponds to sub-<participant_label> from the BIDS spec '
+                                                '(so it does not include "sub-"). If this parameter is not '
+                                                'provided all subjects should be analyzed. Multiple '
+                                                'participants can be specified with a space separated list.',
+                    nargs="+")
+parser.add_argument('--session_label', help='The label of the session that should be analyzed. The label '
+                                            'corresponds to ses-<session_label> from the BIDS spec '
+                                            '(so it does not include "ses-"). If this parameter is not '
+                                            'provided all sessions should be analyzed. Multiple '
+                                            'sessions can be specified with a space separated list.',
+                    nargs="+")
+parser.add_argument('--mri_crop_step',
+                    help='Which step of the crop_fill pipeline should be performed',
+                    choices=["crop", "fill", "zero_pad", "trim"]
+                    )
+parser.add_argument('--acquisition_label',
+                    help='If the dataset contains multiple T1 weighted images from different acquisitions which one '
+                         'should be used? Corresponds to "acq-<acquisition_label>"')
+parser.add_argument('--filling_label',
+                    help='Label of the image that is going to be used for filling. Corresponds to "acq-<filling_label>"')
+parser.add_argument('--zeropad_label',
+                    help='Label of the image that is going to be used for reference to zeropad. Corresponds to '
+                         '"acq-<zeropad_label>"')
+
+args = parser.parse_args()
+
+subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+
+# Got to combine acq_tpl and rec_tpl
+if args.acquisition_label:
+    ar_tpl = "*acq-%s*" % args.acquisition_label
+else:
+    ar_tpl = "*"
+
+# if there are session folders, check if study is truly longitudinal by
+# searching for the first subject with more than one valid sessions
+
+if glob(os.path.join(args.bids_dir, "sub-*", "ses-*")):
+    subjects = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+    for subject_label in subjects:
+        session_dirs = glob(os.path.join(args.bids_dir, "sub-%s" % subject_label, "ses-*"))
+        sessions = [os.path.split(dr)[-1].split("-")[-1] for dr in session_dirs]
+        n_valid_sessions = 0
+        for session_label in sessions:
+            if glob(os.path.join(args.bids_dir, "sub-%s" % subject_label,
+                                 "ses-%s" % session_label,
+                                 "anat",
+                                 "%s_T1w.nii*" % ar_tpl)):
+                n_valid_sessions += 1
+
+subjects_to_analyze = []
+# only for a subset of subjects
+if args.participant_label:
+    subjects_to_analyze = args.participant_label
+    print("Subject to analyze: ", subjects_to_analyze)
+# for all subjects
+else:
+    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+    subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+
+for subject_label in subjects_to_analyze:
+    if glob(os.path.join(args.bids_dir, "sub-%s" % subject_label, "ses-*")):
+        T1s = glob(os.path.join(args.bids_dir,
+                                "sub-%s" % subject_label,
+                                "ses-*",
+                                "anat",
+                                "%s_T1w.nii*" % ar_tpl))
+        sessions = set([os.path.normpath(t1).split(os.sep)[-3].split("-")[-1] for t1 in T1s])
+        if args.session_label:
+            sessions = sessions.intersection(args.session_label)
+        for session_label in sessions:
+            T1s = glob(os.path.join(args.bids_dir,
+                                    "sub-%s" % subject_label,
+                                    "ses-%s" % session_label,
+                                    "anat",
+                                    "%s_T1w.nii*" % ar_tpl))
+            input_args_t1 = ""
+            output_args_t1_crop = ""
+            output_args_t1_fill = ""
+            for T1 in T1s:
+                input_args_t1 += "%s" % T1
+                out = input_args_t1.split("acq-{acq}".format(acq=args.acquisition_label))
+                output_args_t1_crop = "{out1}acq-cropped{out2}".format(out1=out[0], out2=out[1])
+                output_args_t1_filled = "{out1}acq-filled{out2}".format(out1=out[0], out2=out[1])
+                output_args_t1_zeropadded = "{out1}acq-zeropadded{out2}".format(out1=out[0], out2=out[1])
+                input_args_t1_fill = "{out1}acq-{fill}{out2}".format(out1=out[0], fill=args.filling_label, out2=out[1])
+                input_args_t1_zeropad = "{out1}acq-{zeropad}{out2}".format(out1=out[0], zeropad=args.zeropad_label, out2=out[1])
+                input_args_t1_ds = "{out1}acq-ds{out2}".format(out1=out[0], out2=out[1])
+                output_args_t1_trimmed = "{out1}acq-trimmed{out2}".format(out1=out[0], out2=out[1])
+
+            if args.mri_crop_step == "crop":
+                print("Cropping")
+                crop_img(input_args_t1, output_args_t1_crop)
+            elif args.mri_crop_step == "fill":
+                print("Filling")
+                collage_img(input_args_t1, input_args_t1_fill, output_args_t1_filled)
+            elif args.mri_crop_step == "zero_pad":
+                print("Zero padding")
+                zero_pad(input_args_t1, input_args_t1_zeropad, output_args_t1_zeropadded)
+            elif args.mri_crop_step == "trim":
+                print("Trimming")
+                trim(input_args_t1, input_args_t1_ds, output_args_t1_trimmed)
